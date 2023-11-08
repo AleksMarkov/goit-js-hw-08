837c0b870293f99b516a07b8d91b276936fb58a5
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2023-11-07 23:28:35 -0500

    task2
    solved task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,19 +7,19 @@ const player = new Player(iframe);
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(data) {
-  const name = 'videoplayer-current-time';
-  const time = data.seconds;
-  saveTolS(name, time);
+  const frame = 'videoplayer-current-time';
+  const timeSec = data.seconds;
+  localStorsave(frame, timeSec);
 }
-const newTime = Number(getFromLS('videoplayer-current-time'));
+const newTime = Number(localStorget('videoplayer-current-time'));
 player.setCurrentTime(newTime);
 
-function saveTolS(key, value) {
-  localStorage.setItem(key, JSON.stringify(value));
+function localStorsave(frame, timeSec) {
+  localStorage.setItem(frame, JSON.stringify(timeSec));
 }
 
-function getFromLS(key) {
-  const data = localStorage.getItem(key);
+function localStorget(frame) {
+  const data = localStorage.getItem(frame);
   try {
     return JSON.parse(data);
   } catch {
