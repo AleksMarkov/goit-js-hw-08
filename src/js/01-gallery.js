3b2d2de4ffc0d91670d4ef8c9190510027791c72
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2023-11-07 22:22:28 -0500

    updated

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,58 @@
+// Описаний в документації
+//import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+//import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const cont = document.querySelector('.gallery');
+cont.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+cont.addEventListener('click', onGalleryItemClick);
 
-console.log(galleryItems);
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  const imgGallery = event.target;
+  const original = imgGallery.dataset.source;
+  const description = imgGallery.alt;
+
+  const instance = basicLightbox.create(
+    `<div>
+      <img  src="${original}" alt="${description}" width="1140" />
+    </div>`,
+    {
+      onShow: () => {
+        window.addEventListener('keydown', onEscPress);
+      },
+      onClose: () => {
+        window.removeEventListener('keydown', onEscPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscPress(clk) {
+    if (clk.key === 'Escape') {
+      instance.close();
+    }
+  }
+}
+
+function createMarkup(array) {
+  return array
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join('');
+}
